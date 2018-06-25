cd3f23b910549aacb2dd08735896b1506f129114
Anton <anton@adc.sh>
2018-06-25 04:54:53 +0100

    test getStructure

--- test/spec/lib.js
+++ test/spec/lib.js
@@ -1,29 +1,29 @@
 import { equal, throws, assert } from 'zoroaster/assert'
 import mnpIdio from 'mnp-idio'
 import mnpPackage from 'mnp-package'
-import { findStructure } from '../../src/lib'
+import { getStructure } from '../../src/lib'
 
 export default {
   'finds a default structure'() {
-    const res = findStructure(['node', 'mnp', 'test'])
-    const expected = `${mnpPackage}/`
+    const res = getStructure()
+    const expected = `${mnpPackage}`
     equal(res, expected)
   },
   'throws an error when -s is the last argument'() {
     assert.throws(() => {
-      findStructure(['node', 'mnp', 'test', '-s'])
-    })
+      getStructure(null)
+    }, /Could not require structure "null"/)
   },
   'finds an idio structure'() {
-    const res = findStructure(['node', 'mnp', 'test', '-s', 'idio'])
-    const expected = `${mnpIdio}/`
+    const res = getStructure('idio')
+    const expected = `${mnpIdio}`
     equal(res, expected)
   },
   async 'throws an error when structure could not be required'() {
     const name = 'preact'
     await throws({
-      fn: findStructure,
-      args: [['node', 'mnp', 'test', '-s', name]],
+      fn: getStructure,
+      args: [name],
       message: `Could not require structure "${name}".`,
     })
   },
