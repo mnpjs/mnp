09b31007b8db96f0eca771591a0640cb51939c2e
Anton <anton@adc.sh>
2018-06-25 06:46:56 +0100

    build

--- build/lib/index.js
+++ build/lib/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.findStructure = exports.getStructure = void 0;
+exports.getStructure = void 0;
 
 const error = text => {
   const err = new Error(text);
@@ -22,33 +22,4 @@ const getStructure = (structure = 'package') => {
 };
 
 exports.getStructure = getStructure;
-
-const findStructure = (argv = []) => {
-  const i = argv.indexOf('-s');
-  const argFound = i > -1;
-
-  if (argFound && i == argv.length - 1) {
-    throw new Error('Please pass the structure name');
-  }
-
-  const argValueFound = argFound && i != argv.length - 1;
-  let structurePath;
-
-  if (!argValueFound) {
-    structurePath = require('mnp-package');
-  } else {
-    const arg = argv[i + 1];
-    const moduleName = `mnp-${arg}`;
-
-    try {
-      structurePath = require(moduleName); // structures must export a string
-    } catch (err) {
-      error(`Could not require structure "${arg}".`);
-    }
-  }
-
-  return `${structurePath}/`;
-};
-
-exports.findStructure = findStructure;
 //# sourceMappingURL=index.js.map
