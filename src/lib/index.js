43ba7774d1bc70b4db679a5fa6824a9a64630691
Anton <anton@adc.sh>
2018-06-25 04:54:48 +0100

    remove findStructure

--- src/lib/index.js
+++ src/lib/index.js
@@ -12,25 +12,3 @@ export const getStructure = (structure = 'package') => {
     error(`Could not require structure "${structure}".`)
   }
 }
-
-export const findStructure = (argv = []) => {
-  const i = argv.indexOf('-s')
-  const argFound = i > -1
-  if (argFound && i == argv.length -1) {
-    throw new Error('Please pass the structure name')
-  }
-  const argValueFound = argFound && i != argv.length - 1
-  let structurePath
-  if (!argValueFound) {
-    structurePath = require('mnp-package')
-  } else {
-    const arg = argv[i + 1]
-    const moduleName = `mnp-${arg}`
-    try {
-      structurePath = require(moduleName) // structures must export a string
-    } catch (err) {
-      error(`Could not require structure "${arg}".`)
-    }
-  }
-  return `${structurePath}/`
-}
