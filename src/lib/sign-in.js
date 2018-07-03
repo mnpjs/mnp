278d07640cfc66bb03643bdc4ba9b1d3f68afb61
Anton <anton@adc.sh>
2018-07-03 15:48:46 +0100

    local africa

--- src/lib/sign-in.js
+++ src/lib/sign-in.js
@@ -1,38 +1,13 @@
 import africa from 'africa'
-import bosom from 'bosom'
+// import { debuglog } from 'util'
 import questions from '../bin/questions'
-import { homedir } from 'os'
-import { resolve } from 'path'
-import { debuglog } from 'util'
 
-const LOG = debuglog('mnp')
+// const LOG = debuglog('mnp')
 
-export default async function signIn(local = false, force = false) {
-  let r
-  if (local) {
-    try {
-      r = await bosom(resolve(homedir(), '.mnprc'))
-    } catch (er) {
-      LOG('Could not read the global rc file.')
-    }
-  }
-  const c = {
-    ...(local ? { homedir: '.' } : {}),
-    ...(force ? { force } : {}),
-  }
-
-  const q = r ? Object.keys(questions).reduce((a, k) => {
-    const v = questions[k]
-    const o = {
-      ...a,
-      [k]: {
-        ...v,
-        defaultValue: r[k],
-      },
-    }
-    return o
-  }, {}) : questions
-
-  const conf = await africa('mnp', q, c)
+export default async function signIn(force = false) {
+  const conf = await africa('mnp', questions, {
+    force,
+    local: true,
+  })
   return conf
 }
